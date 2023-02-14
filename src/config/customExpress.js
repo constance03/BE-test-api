a5966e6e586762e5e46f4f29f3cd45cd12967f97
constance03 <maria-constance@hotmail.com>
2023-02-14 18:37:52 -0300

    feat: creating and testing the endpoints

---
+++ src/config/customExpress.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const consign = require("consign");
+
+module.exports = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  consign().include("./src/controller").into(app);
+
+  return app;
+};
